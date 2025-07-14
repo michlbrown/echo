abaa088ddd134838b6e4fdc0c687da244e555367
Michael Brown <michlfbrown@gmail.com>
2025-07-14 12:34:55 -0700

    feat: :sparkles: add discord.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from Aux!")
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 
-if __name__ == "__main__":
-    main()
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+client.run(os.environ["DISCORD_TOKEN"])
